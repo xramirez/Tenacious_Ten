0579080166a55fd06ec2f6066ddfed7572b19b27
Basilio <basilio.barron@yahoo.com>
2018-10-30 20:33:25 -0500

    .

--- Tenacious Ten/Assets/Scripts/Leo-Scripts/CharacterController2D.cs
+++ Tenacious Ten/Assets/Scripts/Leo-Scripts/CharacterController2D.cs
@@ -110,8 +110,8 @@ public class CharacterController2D : MonoBehaviour
             // Move the character by finding the target velocity
             Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
             // And then smoothing it out and applying it to the character
-            
-            m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+            //transform.Translate(targetVelocity);
+            m_Rigidbody2D.velocity = targetVelocity;//Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
             
             // If the input is moving the player right and the player is facing left...
             if (move > 0 && !m_FacingRight)

4ba26f8176a3084210004b8bfd71bfb8865b8b40
drschuet <38334969+drschuet@users.noreply.github.com>
2018-11-01 14:11:03 -0500

    Add files via upload

---
+++ Tenacious Ten/Assets/Scripts/Daniel Scripts/DiamondMove.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiamondMove : MonoBehaviour {
+
+	Vector2 mover = new Vector2(-1f, -1f);
+	[SerializeField]
+	float moveSpeed = 5f;
+	Vector2 startingPosition;
+	[SerializeField]
+	float ydistance;
+	[SerializeField]
+	float xdistance;
+
+	// Use this for initialization
+	void Start()
+	{
+		startingPosition = transform.position;
+	}
+
+	void FixedUpdate()
+	{
+		transform.Translate(mover * moveSpeed * Time.deltaTime);
+		if (transform.position.y >= startingPosition.y + ydistance)
+		{
+			mover.y *= -1;
+		}
+		if (transform.position.y <= startingPosition.y - ydistance)
+		{
+			mover.y *= -1;
+		}
+		if (transform.position.x >= startingPosition.x + xdistance)
+		{
+			mover.x *= -1;
+		}
+		if (transform.position.x <= startingPosition.x - xdistance)
+		{
+			mover.x *= -1;
+		}
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.tag == "EnemyBounceWall")
+		{
+			moveSpeed *= -1;
+		}
+	}
+}
---
+++ Tenacious Ten/Assets/Scripts/Daniel Scripts/MoveReverseXonCollision.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveReverseXonCollision : MonoBehaviour {
+
+	[SerializeField]
+	Vector2 mover = new Vector2(-1f, 0);
+	float moveSpeed = 5f;
+	Vector2 startingPosition;
+
+	// Use this for initialization
+	void Start () {
+		startingPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate()
+	{
+		transform.Translate(mover * moveSpeed * Time.deltaTime);
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if(collision.gameObject.tag == "EnemyBounceWall")
+			mover *= -1;
+	}
+}

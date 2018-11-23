5fdff09a9f19d0b844f68257ef78c5b40c3bf5ab
Napoleon de Mesa <napdemesa89@gmail.com>
2018-11-23 00:10:52 -0600

    "cinematic" transition scenes
    added transition scenes for 3 --> 4, and 4 --> 5

---
+++ Tenacious Ten/Assets/Scripts/Sean-Scripts/Transition Scripts/BunjimanOutOfChest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunjimanOutOfChest : MonoBehaviour {
+
+    [SerializeField] float GrowSpeed;
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        this.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 700f));
+        anim = GetComponent<Animator>();
+        anim.SetInteger("State", 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(transform.localScale.x <= 10f)
+        {
+            transform.localScale = new Vector3(transform.localScale.x + GrowSpeed, transform.localScale.y + GrowSpeed, 0f);
+        }
+	}
+}

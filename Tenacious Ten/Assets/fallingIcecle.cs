8d55c2f99ac4a7324e3e182190889f996b2c6ad3
Basilio <basilio.barron@yahoo.com>
2018-11-24 15:34:08 -0600

    Level 3 updated with falling Icecles

---
+++ Tenacious Ten/Assets/fallingIcecle.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fallingIcecle : MonoBehaviour {
+    Rigidbody2D body;
+    public float delay;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        body = GetComponent<Rigidbody2D>();
+        body.velocity = new Vector2(0, -5);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Equals("Knight") || collision.gameObject.name.Equals("Player 2.0"))
+        {
+            Debug.Log("Player Hit");
+            Destroy(gameObject);
+        }
+        if (collision.gameObject.name.Equals("Ground")||collision.gameObject.name.Equals("Ice"))
+        {
+            //Debug.Log("Hit the ground");
+            Destroy(gameObject);
+        }
+    }
+
+}

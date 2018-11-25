da8b07a8fecd78711ee7b817a17a2cfeda7569ec
Napoleon de Mesa <napdemesa89@gmail.com>
2018-11-24 20:21:19 -0600

    Tweaking of boss 4
    victory screen works, and  phase 4 abilities stop upon death of boss

---
+++ Tenacious Ten/Assets/Scripts/Sean-Scripts/Puppet Boss/DestroyOnBoss4Death.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnBoss4Death : MonoBehaviour {
+
+    PuppetBossManager BM;
+    bool fightComplete;
+
+	// Use this for initialization
+	void Start () {
+        BM = GameObject.FindObjectOfType<PuppetBossManager>();
+        fightComplete = false;
+	}
+	
+	// Update is called once per frame
+	void Update() {
+
+        if (GameObject.FindObjectOfType<PuppetBossManager>() != null)
+        {
+            BM = GameObject.FindObjectOfType<PuppetBossManager>();
+        }
+        
+        
+
+        if(BM.GetComponent<EnemyHealthManager>().enemyHealth <= 0)
+        { 
+            Destroy(gameObject);
+        }
+
+	}
+}

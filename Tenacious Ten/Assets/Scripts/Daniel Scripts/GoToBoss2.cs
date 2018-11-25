9bf9c0ff6a8477cbf10797947171352971f247bf
drschuet <38334969+drschuet@users.noreply.github.com>
2018-11-24 21:05:13 -0600

    Level 2 Connecting

---
+++ Tenacious Ten/Assets/Scripts/Daniel Scripts/GoToBoss2.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToBoss2 : MonoBehaviour
+{
+	string scenename = "Boss Fight 02";
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.tag == "Player")
+		{
+			SceneManager.LoadScene(scenename);
+			//SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			SceneManager.LoadScene(scenename);
+			//SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		}
+	}
+	void OnTriggerStay2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			SceneManager.LoadScene(scenename);
+			//SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		}
+	}
+}

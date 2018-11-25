f7b4ecb62d28a43a1706aee2338c17f5a17a4a0e
Xavier Ramirez <xeramirez1997@gmail.com>
2018-11-25 11:13:28 -0600

    Level 4 Actual Final Update
    Probably not the actual final update, I need to add some enemies.
    
    Also added Boss 5 to build settings, so that the door in level 5 actually transitions to the boss fight

--- Tenacious Ten/Assets/Scripts/Sean-Scripts/FinalBossDoor.cs
+++ Tenacious Ten/Assets/Scripts/Sean-Scripts/FinalBossDoor.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinalBossDoor : MonoBehaviour {
 
     StartOrResetLevel SORL;
     Animator anim;
     BoxCollider2D bc;
+    string scenename = "Boss Fight 05";
 
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         SORL = GameObject.FindObjectOfType<StartOrResetLevel>();
         anim = GetComponent<Animator>();
         bc = GetComponent<BoxCollider2D>();
@@ -32,5 +34,34 @@ public class FinalBossDoor : MonoBehaviour {
     {
         yield return new WaitForSeconds(time);
         bc.enabled = true;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("hello");
+        if (collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(scenename);
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        print("hello");
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(scenename);
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+    void OnTriggerStay2D(Collider2D other)
+    {
+        print("hello");
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(scenename);
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
     }
 }

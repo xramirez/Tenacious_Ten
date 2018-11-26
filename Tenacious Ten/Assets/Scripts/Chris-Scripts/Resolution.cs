951dfb12258ec76881c4ed25b75f3ce0ef4223a7
Christopher Dang <dchrisu@yahoo.com>
2018-11-26 00:22:45 -0600

    Resolution is consistent so our game is pretty :)

---
+++ Tenacious Ten/Assets/Scripts/Chris-Scripts/Resolution.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resolution : MonoBehaviour {
+    private Rect SafeScreen;
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log(Screen.width + "x" + Screen.height);
+        Debug.Log(Screen.fullScreen);
+
+        float scalar = 2.1f;
+        int Width = (int)(Screen.height * scalar);
+        if (Width > Screen.width)
+        {
+            Debug.Log("Exception: Lowest possible width updated Screen)");
+            float smallestHeight = Screen.currentResolution.width / scalar;
+            float smallestWidth = smallestHeight * scalar;
+
+            Debug.Log("Exception: Lowest possible width updated Screen = (" + Width + ", " + SafeScreen.height + ")");
+            Screen.SetResolution((int)smallestWidth, (int)smallestHeight, Screen.fullScreen);
+        }
+        else
+        {
+            Debug.Log("Updated Screen = (" + Width + ", " + Screen.height + ")");
+            Screen.SetResolution(Width, (int)Screen.height, Screen.fullScreen);
+        }
+    }
+}

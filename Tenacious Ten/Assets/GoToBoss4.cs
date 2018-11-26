034a3dd09053e8e467e9da6b09dd11b8c5db500f
Xavier Ramirez <xeramirez1997@gmail.com>
2018-11-25 20:17:38 -0600

    Level 4.0 Actual Actual Final Update
    Added more enemy types, and added an animated door at the end.

--- Tenacious Ten/Assets/GoToBoss4.cs
+++ Tenacious Ten/Assets/GoToBoss4.cs
@@ -5,7 +5,38 @@ using UnityEngine.SceneManagement;
 
 public class GoToBoss4 : MonoBehaviour
 {
+    StartOrResetLevel SORL;
+    Animator anim;
+    BoxCollider2D bc;
     string scenename = "Boss Fight 04";
+
+
+    // Use this for initialization
+    void Start()
+    {
+        SORL = GameObject.FindObjectOfType<StartOrResetLevel>();
+        anim = GetComponent<Animator>();
+        bc = GetComponent<BoxCollider2D>();
+        bc.enabled = false;
+        anim.enabled = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (SORL.StartFight)
+        {
+            anim.enabled = true;
+            StartCoroutine(waitForTrigger(1.75f));
+        }
+    }
+
+    IEnumerator waitForTrigger(float time)
+    {
+        yield return new WaitForSeconds(time);
+        bc.enabled = true;
+    }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
@@ -19,18 +50,18 @@ public class GoToBoss4 : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
+        print("hello");
         if (other.gameObject.tag == "Player")
         {
-            print("hello");
             SceneManager.LoadScene(scenename);
             //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
     void OnTriggerStay2D(Collider2D other)
     {
+        print("hello");
         if (other.gameObject.tag == "Player")
         {
-            print("hello");
             SceneManager.LoadScene(scenename);
             //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }

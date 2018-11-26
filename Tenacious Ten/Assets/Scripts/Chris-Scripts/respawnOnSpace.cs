fb253ad550faa2cfe1c3f69195fdbb5649f0a2a1
Christopher Dang <dchrisu@yahoo.com>
2018-11-26 16:11:58 -0600

    Door Fading script for clarity

--- Tenacious Ten/Assets/Scripts/Chris-Scripts/respawnOnSpace.cs
+++ Tenacious Ten/Assets/Scripts/Chris-Scripts/respawnOnSpace.cs
@@ -5,22 +5,51 @@ using UnityEngine.SceneManagement;
 
 public class respawnOnSpace : MonoBehaviour {
     public GameObject HUD;
-
-    void Awake()
+    public bool spacebar;
+    public bool available = false;
+    public bool once = false;
+    
+    void LateUpdate()
     {
-    }
-	void Update()
-    {
+        //if (Input.GetKeyDown("space") && available == true)
         if (Input.GetKeyDown("space"))
-        {
-            //if (SceneManager.GetActiveScene().name.Contains("Level"))
-            //{
-                HUD.GetComponent<Level1DeathMenu>().Respawn();
-            //}
-            //else if(SceneManager.GetActiveScene().name.Contains("Boss Fight"))
-            //{
-                //HUD.GetComponent<Level1DeathMenu>().Respawn();
-            //}
+            {
+            //spacebar = true;
+            HUD.GetComponent<Level1DeathMenu>().Respawn();
+            //available = false;
+            //once = false;
         }
     }
+    /*
+    void Update()
+    {
+        if(once == false)
+        {
+            once = true;
+            Debug.Log("Start OnEnable");
+            Invoke("boolswitch", 2);
+        }
+    }
+
+    void boolswitch()
+    {
+        Debug.Log("Start boolswitch");
+        available = true;
+    }
+    */
+    /*
+    void Update()
+    {
+        if(once == false)
+        {
+            once = true;
+            Invoke()
+            available = true;
+        }
+    }
+
+    void Wait()
+    {
+        yield WaitForSeconds();
+    }*/
 }

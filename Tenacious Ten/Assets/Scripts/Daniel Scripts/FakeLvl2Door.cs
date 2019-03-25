35df2c3ca905de9a21101e9a6b9bff9b0f724fd4
drschuet <38334969+drschuet@users.noreply.github.com>
2019-03-25 16:34:29 -0500

    Added Some door functions for level 2

---
+++ Tenacious Ten/Assets/Scripts/Daniel Scripts/FakeLvl2Door.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FakeLvl2Door : MonoBehaviour
+{
+
+	Vector2 take = new Vector2(0, 0);
+
+	void Start()
+	{
+		take = GameObject.Find("FalseDoorLetOut").transform.position;
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.tag == "Player")
+		{
+			collision.transform.position = take;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			other.transform.position = take;
+		}
+	}
+	void OnTriggerStay2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			other.transform.position = take;
+		}
+	}
+}

ed41b233ca70b6d038839d17cd3d2ada731278bd
Xavier Ramirez <xeramirez1997@gmail.com>
2019-04-02 18:31:51 -0500

    More sprite masking
    it dark now

--- Tenacious Ten/Assets/BackgroundManager.cs
+++ Tenacious Ten/Assets/BackgroundManager.cs
@@ -1,13 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class BackgroundManager : MonoBehaviour
 {
+    public Tilemap darkMap;
+    public Tilemap blurredMap;
+    public Tilemap backgroundMap;
+
+    public Tile darkTile;
+    public Tile blurredTile;
     // Start is called before the first frame update
     void Start()
     {
-        
+        darkMap.origin = blurredMap.origin = backgroundMap.origin;
+        darkMap.size = blurredMap.size = backgroundMap.size;
+
+        foreach(Vector3Int p in darkMap.cellBounds.allPositionsWithin)
+        {
+            darkMap.SetTile(p, darkTile);
+        }
+
+        foreach (Vector3Int p in blurredMap.cellBounds.allPositionsWithin)
+        {
+            blurredMap.SetTile(p, blurredTile);
+        }
     }
 
     // Update is called once per frame

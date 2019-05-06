e122d3ade842492981db3e7739731b705345ad03
drschuet <38334969+drschuet@users.noreply.github.com>
2019-05-06 10:59:32 -0500

    TankBehaviorAddition

--- Tenacious Ten/Assets/Scripts/Sean-Scripts/Level 6 Scripts/L6TorsoManager.cs
+++ Tenacious Ten/Assets/Scripts/Sean-Scripts/Level 6 Scripts/L6TorsoManager.cs
@@ -25,7 +25,22 @@ public class L6TorsoManager : MonoBehaviour
 
     public Level6Manager L6Manager;
 
-    void Start()
+	[SerializeField] GameObject BouncyBall;
+	Transform EmitLoc;
+	int currentCannon = 1;// 1 2 and 3
+	[SerializeField]
+	bool readyToShoot = false;
+	[SerializeField] float shotTimer;
+	float initShotTimer;
+	[SerializeField] int shotCounter;
+	int reloadNumber;
+	[SerializeField]
+	float reloadTime;
+
+	[SerializeField]
+	AudioSource cannonSound;
+
+	void Start()
     {
         maxHealth = enemyHealth;
         sr = GetComponent<SpriteRenderer>();
@@ -33,6 +48,10 @@ public class L6TorsoManager : MonoBehaviour
         moveToStart = false;
         L6Manager = FindObjectOfType<Level6Manager>();
 		track = GetComponent<ChariotMiniReset3>();
+		EmitLoc = this.gameObject.transform.GetChild(currentCannon);
+		readyToShoot = false;
+		initShotTimer = shotTimer;
+		reloadNumber = shotCounter;
 	}
 
     void FixedUpdate()
@@ -55,9 +74,36 @@ public class L6TorsoManager : MonoBehaviour
             if (transform.position.x <= -53)
             {
                 moveToStart = true;
+				readyToShoot = true;
             }
         }
-    }
+
+		if (readyToShoot)
+		{
+			shotTimer -= Time.deltaTime;
+			if (shotTimer <= 0 && shotCounter >= 0)
+			{
+				cannonSound.Play();
+				shotCounter--;
+				shotTimer = initShotTimer;
+				Instantiate(BouncyBall, EmitLoc.position, Quaternion.identity); // Quaternion.Euler(0f, 0f, 90f));
+				if (currentCannon == 1)
+					currentCannon = 2;
+				else if (currentCannon == 2)
+					currentCannon = 3;
+				else if (currentCannon == 3)
+					currentCannon = 1;
+				EmitLoc = this.gameObject.transform.GetChild(currentCannon);
+			}
+
+			if (shotCounter <= 0)
+			{
+				readyToShoot = false;
+				shotCounter = reloadNumber;
+				StartCoroutine(reload(reloadTime));
+			}
+		}
+	}
 
     public void giveDamage(int damageToGive)
     {
@@ -65,6 +111,12 @@ public class L6TorsoManager : MonoBehaviour
         enemyHealth -= damageToGive;
         StartCoroutine(flash());
     }
+
+	IEnumerator reload(float reloadTime)
+	{
+		yield return new WaitForSeconds(reloadTime);
+		readyToShoot = true;
+	}
 
     IEnumerator flash()
     {

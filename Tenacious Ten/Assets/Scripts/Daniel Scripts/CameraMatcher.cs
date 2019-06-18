c7fedc66e9d4ced5b10c61899f83c717bcfb00f8
drschuet <38334969+drschuet@users.noreply.github.com>
2019-06-17 20:27:22 -0500

    Camera Improvement

---
+++ Tenacious Ten/Assets/Scripts/Daniel Scripts/CameraMatcher.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMatcher : MonoBehaviour
+{
+
+	Vector2 TrackerPosition = new Vector2(0, 0);
+	[SerializeField]
+	GameObject player;
+	float zed;
+
+	// Use this for initialization
+	void Start()
+	{
+		zed = transform.position.z;
+	}
+
+	void FixedUpdate()
+	{
+		TrackerPosition.x = player.transform.position.x;
+		TrackerPosition.y = player.transform.position.y;
+		transform.position = new Vector3(TrackerPosition.x,TrackerPosition.y,zed);
+	}
+}
+

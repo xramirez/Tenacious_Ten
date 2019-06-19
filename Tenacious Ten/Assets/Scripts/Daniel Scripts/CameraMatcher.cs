88ff780c02261775cd731744382e8bc51ac63c6a
drschuet <38334969+drschuet@users.noreply.github.com>
2019-06-18 21:25:44 -0500

    Camera Changes

--- Tenacious Ten/Assets/Scripts/Daniel Scripts/CameraMatcher.cs
+++ Tenacious Ten/Assets/Scripts/Daniel Scripts/CameraMatcher.cs
@@ -9,6 +9,17 @@ public class CameraMatcher : MonoBehaviour
 	[SerializeField]
 	GameObject player;
 	float zed;
+	[SerializeField]
+	bool matchHorizontal = true;
+	[SerializeField]
+	bool matchVertical = true;
+	float distanceY;
+	float distanceX;
+	[SerializeField]
+	float moveSpeed;
+	float mover;
+	[SerializeField]
+	float divider;
 
 	// Use this for initialization
 	void Start()
@@ -18,9 +29,26 @@ public class CameraMatcher : MonoBehaviour
 
 	void FixedUpdate()
 	{
-		TrackerPosition.x = player.transform.position.x;
-		TrackerPosition.y = player.transform.position.y;
-		transform.position = new Vector3(TrackerPosition.x,TrackerPosition.y,zed);
+		if (matchHorizontal && matchVertical)
+		{
+			TrackerPosition.x = player.transform.position.x;
+			TrackerPosition.y = player.transform.position.y;
+			transform.position = new Vector3(TrackerPosition.x, TrackerPosition.y, zed);
+		}
+		else if(matchVertical)//not matching horizontal
+		{
+			distanceX = player.transform.position.x - transform.position.x;
+			distanceY = player.transform.position.y - transform.position.y;
+			mover = distanceX * moveSpeed / divider;
+			transform.position += new Vector3(mover, distanceY, 0);
+		}
+		else if(matchHorizontal)//not matching vertical
+		{
+			distanceX = player.transform.position.x - transform.position.x;
+			distanceY = player.transform.position.y - transform.position.y;
+			mover = distanceX * moveSpeed / divider;
+			transform.position += new Vector3(distanceX, mover, 0);
+		}
 	}
 }
 

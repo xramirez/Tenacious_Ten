13f8e2c4d84a0ec8209374b58edbb3ca00ab1f2f
Christopher Dang <dchrisu@yahoo.com>
2019-07-17 22:02:14 -0500

    Main Menu links to Options / Controls / Volume

---
+++ Tenacious Ten/Assets/Scripts/Chris-Scripts/goToControls.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class goToControls : MonoBehaviour
+{
+    // Use this for initialization
+    public void loadControlsScene()
+    {
+        SceneManager.LoadScene("OptionsControls");
+    }
+}
---
+++ Tenacious Ten/Assets/Scripts/Chris-Scripts/goToVolume.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class goToVolume : MonoBehaviour
+{
+    // Use this for initialization
+    public void loadVolumeScene()
+    {
+        SceneManager.LoadScene("OptionsVolume");
+    }
+}
